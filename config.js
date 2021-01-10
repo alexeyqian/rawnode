9a4b627e5ca68d58539e83f054fe999bdf7b1086
alexeyqian <alexeyqian@gmail.com>
2021-01-10 10:59:16 +0700

    add checks

--- config.js
+++ config.js
@@ -4,14 +4,16 @@ environments.staging = {
     'httpPort': 3000,
     'httpsPort' : 3001,
     'evnName': 'staging',
-    'hashingSecret': 'asfdasltj'
+    'hashingSecret': 'asfdasltj',
+    'maxChecks': 5
 };
 
 environments.production = {
     'httpPort': 5000,
     'httpsPort' : 50001,
     'evnName': 'production',
-    'hashingSecret': 'xdu0urjf;f'
+    'hashingSecret': 'xdu0urjf;f',
+    'maxChecks': 5
 };
 
 var currentEnvironment = type(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
